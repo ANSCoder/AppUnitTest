3b3061fefd87995820a27441d4803999ebbb2f30
ANSCoder <nimjea@gmail.com>
2025-01-25 20:50:12 +0530

    Initial Commit

---
+++ AppUnitTest/AppUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppUnitTestApp.swift
+//  AppUnitTest
+//
+//  Created by Anand Nimje on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AppUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
